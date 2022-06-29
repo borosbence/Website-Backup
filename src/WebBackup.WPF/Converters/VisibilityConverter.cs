7b4c32e7014bbecb3c5078785c11011d2727f387
Bence Boros <borosbence6800@gmail.com>
2022-06-29 17:09:31 +0200

    hide context-menu

--- src/WebBackup.WPF/Converters/VisibilityConverter.cs
+++ src/WebBackup.WPF/Converters/VisibilityConverter.cs
@@ -6,7 +6,7 @@ using System.Windows.Data;
 namespace WebBackup.WPF.Converters
 {
     [ValueConversion(typeof(bool), typeof(Visibility))]
-    public class ObjectToVisibilityConverter : IValueConverter
+    public class BoolToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
@@ -16,6 +16,20 @@ namespace WebBackup.WPF.Converters
             }
             bool visible = (bool)value;
             return visible ?  Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [ValueConversion(typeof(object), typeof(Visibility))]
+    public class ObjectToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? Visibility.Visible: Visibility.Hidden;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
